d63ded9c7fc0f65d11614fa665aacbc7e4390843
afsaarhussain149 <afsaarhussain149@gmail.com>
2024-08-27 15:13:23 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AdoConnectionWithDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // cs is variable and declaring database connection
+            string cs = "Server=localhost;Port=5432;Database=ado_dbms;UserId=postgres;Password=1234";
+            NpgsqlConnection conn = new NpgsqlConnection(cs);
+
+            try
+            {
+                conn.Open();
+
+                if (conn.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("Connection is Successfull...!");
+                }
+
+            }
+            catch (NpgsqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            Console.ReadLine();
+        }
+
+        
+    }
+}
